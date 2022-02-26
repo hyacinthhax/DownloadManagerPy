60dbaea6f0d8f0327b6fb8c1caaf187fb8a760cc
Hyacinthax <49726913+hyacinthhax@users.noreply.github.com>
2022-02-26 11:38:04 -0500

    Update Download_Manager.py
    Added Picture settings removed schedule until implementation in server

--- Download_Manager.py
+++ Download_Manager.py
@@ -1,61 +1,66 @@
-#Download Manager
+# Download Manager
 import os
 import shutil
-import schedule
-import time
+# import schedule
+# import time
 
 src = '/home/user/Downloads/'
 dest = '/home/user/'
-posvid = ['.mp4', '.mov', '.avi', '.flv', '.mkv', '.wmv', '.webm'] 
+posvid = ['.mp4', '.mov', '.avi', '.flv', '.mkv', '.wmv', '.webm']
 zipz = ['.zip', '.tar', '.bz2', '.gz']
 teext = ['.txt', '.md', '.asc', '.pdf']
 inst = ['.iso', '.deb']
+pics = ['.jpg', '.jpeg', '.png', '.gif', '.bmp']
 slef = '/Desktop/Testing/Projects/DownloadManager/'
 
 with open('specified.txt', 'r') as specif:
-			text = specif.readlines()
-			print(text)
-			global text
+    global text
+    text = specif.readlines()
+    # print(text)
+
 
 def run():
-	os.chdir(src)
-	for file in os.listdir(src):
-		file_name, file_ext = os.path.splitext(file)
-		print(file_ext)
-		if str(file) in str(text):
-			mark = 'Documents/Specified/'
-			full_dest = os.path.join(dest, mark, file)
-			print(full_dest)
-			print('\n')
-			shutil.move(file,full_dest)
-		elif str(file_ext) in zipz:
-			zeep = 'Documents/Zips/'
-			full_dest = os.path.join(dest, zeep, file)
-			print(full_dest)
-			print('\n')
-			shutil.move(file,full_dest)
-		elif str(file_ext) in teext:
-			text1 = 'Documents/Text/'
-			full_dest = os.path.join(dest, text1, file)
-			print(full_dest)
-			print('\n')
-			shutil.move(file,full_dest)
-		elif str(file_ext) in inst:
-			instf = 'Documents/Installers/'
-			full_dest = os.path.join(dest, instf, file)
-			print(full_dest)
-			print('\n')
-			shutil.move(file,full_dest)
-		elif str(file_ext) in posvid:
-			veed = 'Videos/'
-			full_dest = os.path.join(dest, veed, file)
-			print(full_dest)
-			print('\n')
-			shutil.move(file,full_dest)
+    os.chdir(src)
+    for file in os.listdir(src):
+        file_name, file_ext = os.path.splitext(file)
+        # print(file_ext)
+        if str(file) in str(text):
+            mark = 'Documents/Specified/'
+            full_dest = os.path.join(dest, mark, file)
+            print(full_dest)
+            shutil.move(file, full_dest)
+        elif str(file_ext) in zipz:
+            zeep = 'Documents/Zips/'
+            full_dest = os.path.join(dest, zeep, file)
+            print(full_dest)
+            shutil.move(file, full_dest)
+        elif str(file_ext) in teext:
+            text1 = 'Documents/Text/'
+            full_dest = os.path.join(dest, text1, file)
+            print(full_dest)
+            shutil.move(file, full_dest)
+        elif str(file_ext) in inst:
+            instf = 'Documents/Installers/'
+            full_dest = os.path.join(dest, instf, file)
+            print(full_dest)
+            shutil.move(file, full_dest)
+        elif str(file_ext) in posvid:
+            veed = 'Videos/'
+            full_dest = os.path.join(dest, veed, file)
+            print(full_dest)
+            shutil.move(file, full_dest)
+        elif str(file_ext) in pics:
+            picz = 'Pictures/'
+            full_dest = os.path.join(dest, picz, file)
+            print(full_dest)
+            shutil.move(file, full_dest)
+        else:
+            pass
+
 
 run()
-schedule.every().hour.do(run)
+# schedule.every().hour.do(run)
 
-while True:
-    schedule.run_pending()
-    time.sleep(1)
+# while True:
+    # schedule.run_pending()
+    # time.sleep(1)
